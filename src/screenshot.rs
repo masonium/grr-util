72d1e91ff57d922b5205fc16e1ee9a5e371825d2
Mason Smith <masonium@gmail.com>
2020-06-28 15:52:16 -0700

    screenshot: blit to internal framebuffer to allow for multisampling

--- src/screenshot.rs
+++ src/screenshot.rs
@@ -3,18 +3,61 @@ use std::path::Path;
 
 /// Save the color information for the default framebuffer to disk.
 pub fn save_framebuffer_rgba<P: AsRef<Path>>(
+    src_framebuffer: grr::Framebuffer,
     device: &grr::Device,
     region: grr::Region,
     path: P,
 ) -> Result<(), std::io::Error> {
     let pixel_data_size: usize = (region.w * region.h) as usize * 4;
 
+    // create a non-multisampled framebuffer to blit to.
+    let (img, img_view) = unsafe {
+        device
+            .create_image_and_view(
+                grr::ImageType::D2 {
+                    width: region.w as u32,
+                    height: region.h as u32,
+                    layers: 1,
+                    samples: 1,
+                },
+                grr::Format::R8G8B8A8_UNORM,
+                1,
+            )
+            .unwrap()
+    };
+
+    let fb = unsafe { device.create_framebuffer().unwrap() };
+    unsafe {
+        device.bind_attachments(
+            fb,
+            &[(
+                grr::Attachment::Color(0),
+                grr::AttachmentView::Image(img_view),
+            )],
+        );
+    }
+
     // create a buffer to read the results to.
     let mut buffer_data: Vec<u8> = Vec::with_capacity(pixel_data_size);
     buffer_data.resize(pixel_data_size, 0);
 
     unsafe {
-        device.read_pixels(
+        device.blit(
+            src_framebuffer,
+            region,
+            fb,
+            grr::Region {
+                x: 0,
+                y: 0,
+                w: region.w,
+                h: region.h,
+            },
+            grr::Filter::Linear,
+        );
+
+        device.bind_read_framebuffer(fb);
+
+        device.copy_attachment_to_host(
             region,
             grr::SubresourceLayout {
                 alignment: 1,
@@ -48,5 +91,11 @@ pub fn save_framebuffer_rgba<P: AsRef<Path>>(
         image::ColorType::Rgba8,
     )
     .unwrap();
+
+    unsafe {
+        device.delete_framebuffer(fb);
+        device.delete_image_view(img_view);
+        device.delete_image(img);
+    }
     Ok(())
 }
